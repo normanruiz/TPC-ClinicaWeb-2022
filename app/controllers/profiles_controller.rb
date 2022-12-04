f47e03a0f59fa9c73de05748a12afe90b023d041
Norman Ruiz <norman.ruiz@outlook.com.ar>
2022-12-03 21:41:57 -0300

    Agrego funcionalidades alta baja y modificacion de modelo Perfil.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,39 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
   end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    profile = Profile.new
+    data_profile = params[:profile]
+    profile.name = data_profile[:name]
+    profile.save
+    redirect_to "/profiles/#{profile.id}/show"
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    data_profile = params[:profile]
+    name = data_profile[:name]
+    @profile.update(name: name)
+    redirect_to "/profiles/#{@profile.id}/show"
+  end
+
+  def destroy
+    @profile = Profile.find(params[:id])
+    @profile.destroy
+    redirect_to "/profiles/index"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   get 'profiles/index', to: 'profiles#index'
-  get 'profiles/show'
-  get 'profiles/create'
-  get 'profiles/update'
-  get 'profiles/destroy'
+  get 'profiles/new', to: 'profiles#new'
+  get 'profiles/:id/show', to: 'profiles#show'
+  post 'profiles/create', to: 'profiles#create'
+  get 'profiles/:id/edit', to: 'profiles#edit'
+  patch 'profiles/update', to: 'profiles#update'
+  delete 'profiles/:id/destroy', to: 'profiles#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
